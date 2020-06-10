aadf6bf9fde2f5a32bebd9af579def73463a9084
ConfusedCoder <noahmatthewboyle@gmail.com>
2020-06-10 23:39:11 +0100

    adding exiting program

--- music_creator.py
+++ music_creator.py
@@ -20,7 +20,8 @@ while not done:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
-            done = True
+            quit()
+            
         if event.type == pygame.MOUSEBUTTONDOWN:
             _, mouse_y = pygame.mouse.get_pos()
             pygame.draw.rect(screen, (0,255,0), [x, mouse_y, 10, w])
